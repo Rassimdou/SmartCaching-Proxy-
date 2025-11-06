d0dd1f5a5158043d185fa6ea848469cdd13cf5eb
douaoouria rassim <162766373+Rassimdou@users.noreply.github.com>
2025-11-06 21:53:46 +0100

    implement LRU

---
+++ src/cache/LRU_cache.ts
@@ -0,0 +1,71 @@
+import { CachedResponse } from "./InMemoryCache";
+
+export class MemoryCache {
+    private storage: Map<string, CachedResponse>;
+    private readonly capacity :number;
+
+    constructor(capacity:number = 100){
+        this.storage = new Map()
+        this.capacity = capacity
+    }
+
+    get(key: string): CachedResponse | null {
+
+        const cached = this.storage.get(key);
+
+        //chwck if its expired 
+        if (cached && this.isExpired(cached)){
+            this.storage.delete(key);
+            return null;
+        }
+        else {
+            //delete and reinsert to keep it as MRU (most recently used)
+            this.storage.delete(key); 
+            this.storage.set(key , cached!);
+            return cached || null ;
+        }
+
+    }
+    set(key:string , value:CachedResponse):void {
+        //delete it to re-insert a
+        if (this.storage.has(key))
+            this.storage.delete(key);
+
+        //check capacity 
+        if(this.storage.size >= this.capacity ){ 
+
+            const firstKey = this.storage.keys().next().value;
+            if(firstKey != undefined){
+            this.storage.delete(firstKey);
+            console.log(`Evicted LRU:${firstKey}`)
+            }   
+        }
+        this.storage.set(key , value)
+
+    }
+
+
+    delete(key:string):boolean{
+        return this.storage.delete(key);
+    }
+
+
+    clear():void{
+        this.storage.clear();
+    }
+
+    private isExpired(entry:CachedResponse): boolean {
+        if(!entry.ttl) return false;
+        return Date.now()-entry.timestamp > entry.ttl;  
+    }
+
+    getStats(){
+        return{
+            size: this.storage.size,
+            capacity: this.capacity,
+            keys: Array.from(this.storage.keys())
+        }
+    }
+
+}
+
